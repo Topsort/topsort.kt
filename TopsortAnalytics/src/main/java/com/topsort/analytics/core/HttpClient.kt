e18ff74d1e72b6bc641aa61ee19b86c149f0a5ab
Guilherme Pimenta <guilherme@topsort.com>
2024-07-03 10:17:52 -0300

    chore(sdk): remove retrofit and okhttp (#5)
    * created new http client and service
    * removed retrofit and okhttp

---
+++ TopsortAnalytics/src/main/java/com/topsort/analytics/core/HttpClient.kt
@@ -0,0 +1,106 @@
+package com.topsort.analytics.core
+
+import java.io.Closeable
+import java.io.IOException
+import java.io.OutputStream
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.zip.GZIPOutputStream
+
+const val LIBRARY_VERSION = 1.0
+
+data class HttpResponse(
+    val code : Int,
+    val message : String,
+){
+    fun isSuccessful() : Boolean {
+        return code in 200..299
+    }
+}
+
+class HttpClient (
+    private val apiHost: String,
+    private val requestFactory: RequestFactory = RequestFactory()
+) {
+
+    fun post(body: String, bearerToken: String?) : HttpResponse {
+        val connection: HttpURLConnection = requestFactory.upload(apiHost, bearerToken)
+        val postConnection = connection.createPostConnection()
+        val writeStream = postConnection.outputStream!!.bufferedWriter()
+
+        writeStream.write(body)
+        writeStream.flush()
+        postConnection.outputStream.close()
+
+        val response = HttpResponse(connection.responseCode, connection.responseMessage)
+        postConnection.close()
+
+        return response
+    }
+}
+
+/**
+ * Wraps an HTTP connection. Callers can either read from the connection via the [ ] or write to the connection via [OutputStream].
+ */
+abstract class Connection(
+    private val connection: HttpURLConnection,
+    val outputStream: OutputStream?
+) : Closeable {
+    @Throws(IOException::class)
+    override fun close() {
+        connection.disconnect()
+    }
+}
+
+internal fun HttpURLConnection.createPostConnection(): Connection {
+    val encoding = getRequestProperty("Content-Encoding") ?: ""
+    val outputStream: OutputStream =
+        if (encoding.contains("gzip")) {
+            GZIPOutputStream(this.outputStream)
+        }
+        else {
+            this.outputStream
+        }
+    return object : Connection(this, outputStream) {
+        @Throws(IOException::class)
+        override fun close() {
+            super.close()
+            this.outputStream?.close()
+        }
+    }
+}
+
+class RequestFactory {
+
+    fun upload(apiHost: String, bearerToken : String?): HttpURLConnection {
+        val connection: HttpURLConnection = openConnection("http://$apiHost/")
+        connection.requestMethod = "POST"
+        bearerToken?.let {
+            connection.setRequestProperty("Authorization", "Bearer $bearerToken")
+        }
+        connection.setRequestProperty("Accept", "application/json")
+        connection.setRequestProperty("Content-Type", "application/json; charset=utf-8")
+        connection.setRequestProperty(
+            "User-Agent",
+            "topsort.kt/$LIBRARY_VERSION"
+        )
+        connection.doOutput = true
+        connection.setChunkedStreamingMode(0)
+        return connection
+    }
+
+    private fun openConnection(url: String): HttpURLConnection {
+        val requestedURL: URL = try {
+            URL(url)
+        } catch (e: MalformedURLException) {
+            val error = IOException("Attempted to use malformed url: $url", e)
+            throw error
+        }
+        val connection = requestedURL.openConnection() as HttpURLConnection
+        connection.connectTimeout = 15_000 // 15s
+        connection.readTimeout = 20_1000 // 20s
+        //connection.doInput = true
+        return connection
+    }
+}
---
+++ TopsortAnalytics/src/main/java/com/topsort/analytics/service/TopsortAnalyticsHttpService.kt
@@ -0,0 +1,49 @@
+package com.topsort.analytics.service
+
+import com.google.gson.Gson
+import com.topsort.analytics.Cache
+import com.topsort.analytics.core.HttpClient
+import com.topsort.analytics.core.HttpResponse
+import com.topsort.analytics.model.ClickEvent
+import com.topsort.analytics.model.ImpressionEvent
+import com.topsort.analytics.model.PurchaseEvent
+
+private const val apiUrl = "https://api.topsort.com/v1/events"
+
+internal object TopsortAnalyticsHttpService {
+
+    val httpClient: HttpClient = HttpClient(apiUrl)
+
+    val service: Service = buildService()
+
+    private val gson = Gson()
+
+    private fun buildService(): Service {
+        return object : Service {
+            private fun reportEvent(event: Any): HttpResponse {
+                val json = gson.toJson(event)
+                return httpClient.post(json, Cache.token.ifEmpty { null })
+            }
+
+            override fun reportImpression(impressionEvent: ImpressionEvent): HttpResponse {
+                return reportEvent(impressionEvent)
+            }
+
+            override fun reportClick(clickEvent: ClickEvent): HttpResponse {
+                return reportEvent(clickEvent)
+            }
+
+            override fun reportPurchase(purchaseEvent: PurchaseEvent): HttpResponse {
+                return reportEvent(purchaseEvent)
+            }
+        }
+    }
+
+    interface Service {
+        fun reportImpression(impressionEvent: ImpressionEvent): HttpResponse
+
+        fun reportClick(clickEvent: ClickEvent): HttpResponse
+
+        fun reportPurchase(purchaseEvent: PurchaseEvent): HttpResponse
+    }
+}
