bb9cdce72d644e890d7d72899539df7e76e91bff
Felipe Contreras-Salinas <fcs@topsort.com>
2024-08-06 16:36:27 -0400

    fix missing ,

--- TopsortAnalytics/src/main/java/com/topsort/analytics/banners/BannerResponse.kt
+++ TopsortAnalytics/src/main/java/com/topsort/analytics/banners/BannerResponse.kt
@@ -4,7 +4,7 @@ import com.topsort.analytics.model.auctions.EntityType
 
 data class BannerResponse(
     val id: String,
-    val type: EntityType
+    val type: EntityType,
     val url: String,
     val resolvedBidId: String,
 ) {}
