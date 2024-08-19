cd0327eb79b027021971876a5da49892eef9035e
Guilherme Pimenta <guilherme@topsort.com>
2024-08-19 10:56:13 -0300

    Device enum to uppercase

--- TopsortAnalytics/src/main/java/com/topsort/analytics/model/auctions/AuctionRequest.kt
+++ TopsortAnalytics/src/main/java/com/topsort/analytics/model/auctions/AuctionRequest.kt
@@ -1,11 +1,16 @@
 package com.topsort.analytics.model.auctions
 
+import org.json.JSONArray
 import org.json.JSONObject
 
 data class AuctionRequest(
     val auctions : List<Auction>
 ){
     fun toJsonObject(): JSONObject {
-        return JSONObject().put("auctions", auctions)
+        val array = JSONArray()
+        auctions.indices.map {
+            array.put(it, auctions[it].toJsonObject())
+        }
+        return JSONObject().put("auctions", array)
     }
 }
